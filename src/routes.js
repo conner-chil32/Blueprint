94f80286d8958fe3f661ee03132266e6b6459c1a
bomrr <cparsons2@csus.edu>
2025-03-15 18:54:40 -0700

    Addition for Scrum 155 and 156, setting up the routing for the project view and the user web backend pages.

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,31 @@
  * Registers all routes for the app
  * @param {Express} app - The express app to register the routes on
  */
+
+const express = require('express');
+const path = require('path');
+
 function registerRoutes(app) {
+    app.use(express.static(path.join(__dirname, 'view')));
 
     //TODO: SCRUM-47'S tasks for routing will mainly be implemented here
-
 
     //  !!!     ROUTES     !!!
     app.get('/', (req, res) => {
         console.log('GET / sending ./res/index.html');
         res.sendFile('./view/index.html', { root: __dirname });
+    });
+
+    // Project View
+    app.get('/ProjectView', (req, res) => {
+        console.log('GET /ProjectView sending ./res/ProjectView.html');
+        res.sendFile('./view/ProjectView.html', { root: __dirname });
+    });
+
+    // User Website Backend, shows statistics on their site
+    app.get('/UserWebBackend', (req, res) => {
+        console.log('GET /UserWebBackend sending ./res/UserWebBackend.html');
+        res.sendFile('./view/UserWebBackend.html', { root: __dirname });
     });
 
 }
