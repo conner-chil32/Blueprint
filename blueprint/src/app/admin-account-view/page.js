9cc0fdd07dc724831e027efcb3e15c96a2d8fc70
staticBytess <160567397+staticBytess@users.noreply.github.com>
2025-04-16 18:33:29 -0700

    admin account view page is finished

--- blueprint/src/app/admin-account-view/page.js
+++ blueprint/src/app/admin-account-view/page.js
@@ -14,7 +14,7 @@ export default function admin_view() {
                 <div className={styles.leftSide}>
                     <div className={styles.topRow}>
                         <div className={styles.cell}>
-                            <h1>Server Status Summary:</h1>
+                            <h1><b>Server Status Summary:</b></h1>
                             <dl className = {styles.infoText}>
                                 <dt>Ping: 00ms</dt>
                                 <dt>Mem. Usage: 0%</dt>
@@ -22,7 +22,7 @@ export default function admin_view() {
                             </dl>
                         </div>
                         <div className={styles.cell}>
-                            <h1>Selected User Summary:</h1>
+                            <h1><b>Selected User Summary:</b></h1>
                             <dl className = {styles.infoText}>
                                 <dt>Username</dt>
                                 <dt>Email address</dt>
@@ -31,7 +31,7 @@ export default function admin_view() {
                             </dl>
                         </div>
                         <div className={styles.cell}>
-                            <h1>Admin Actions:</h1>
+                            <h1><b>Admin Actions:</b></h1>
                             <div className={styles.ActionButtonsList}>
                                 <ActionButton label="Delete account" />
                                 <ActionButton label="Temporarily ban user" />
@@ -42,15 +42,15 @@ export default function admin_view() {
                     </div>
                     <div className={styles.bottomRow}>
                         <div className={styles.cell}>
-                            <h1>Server Status</h1>
+                            <h1><b>Server Status:</b></h1>
                             <img src="images/usage_demo.png"></img>
                         </div>
                     </div>
                 </div>
                 <div className={styles.rightSide}>
                     <div className={styles.cell}>
-                        <h1>Modify Accounts:</h1>
-                        <input type="text" placeholder="Search For Account" />
+                        <h1><b>Modify Accounts:</b></h1>
+                        <input className={styles.searchUser} type="text" placeholder="Search For Account" />
                         <dl className={styles.accountList}>
                             <dt>Username</dt>
                             <dt>Username</dt>
