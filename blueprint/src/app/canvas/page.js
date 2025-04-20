efdc1dc56ff13097113c777de5310f16bdf04db4
Jacob Francis <JacobFrancisR1@gmail.com>
2025-04-20 14:59:22 -0700

    fixing canvas page

--- blueprint/src/app/canvas/page.js
+++ blueprint/src/app/canvas/page.js
@@ -1,5 +1,5 @@
 import Navbar from "../components/navbar";
-import styles from './page.module.css';
+import styles from "./page.module.css";
 
 export default function CanvasPage() {
   return (
@@ -8,28 +8,28 @@ export default function CanvasPage() {
       <div className={styles.mainContainer}>
         <div className={styles.leftPanel}>
           <div className={styles.controlMenu}>
-            <button onClick={() => alert('File')}>File</button>
-            <button onClick={() => alert('Edit')}>Edit</button>
-            <button onClick={() => alert('Undo')}>Undo</button>
-            <button onClick={() => alert('Redo')}>Redo</button>
+            <button type="button">File</button>
+            <button type="button">Edit</button>
+            <button type="button">Undo</button>
+            <button type="button">Redo</button>
           </div>
 
           <div className={styles.sectionTitle}>Templates</div>
-          <button className={styles.categoryItem}>Website Templates</button>
-          <button className={styles.categoryItem}>Blog Templates</button>
+          <button className={styles.categoryItem} type="button">Website Templates</button>
+          <button className={styles.categoryItem} type="button">Blog Templates</button>
 
           <div className={styles.divider}></div>
 
           <div className={styles.sectionTitle}>Text Elements</div>
-          <button className={styles.categoryItem}>Headings</button>
-          <button className={styles.categoryItem}>Paragraphs</button>
+          <button className={styles.categoryItem} type="button">Headings</button>
+          <button className={styles.categoryItem} type="button">Paragraphs</button>
 
           <div className={styles.divider}></div>
 
           <div className={styles.sectionTitle}>UI Elements</div>
-          <button className={styles.categoryItem}>Buttons</button>
-          <button className={styles.categoryItem}>Forms</button>
-          <button className={styles.categoryItem}>Images</button>
+          <button className={styles.categoryItem} type="button">Buttons</button>
+          <button className={styles.categoryItem} type="button">Forms</button>
+          <button className={styles.categoryItem} type="button">Images</button>
         </div>
 
         <div className={styles.canvasArea}>
