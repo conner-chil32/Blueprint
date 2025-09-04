852aed718f52632fe0e1219409aa21a9e370b740
David Vigil <160567397+staticBytess@users.noreply.github.com>
2025-09-04 12:57:47 -0700

    Corrected items in form

--- blueprint/src/app/signup/page.js
+++ blueprint/src/app/signup/page.js
@@ -32,27 +32,29 @@ export default function SignUpPage() {
         <Navbar />
             <div className={styles.body}>
                 <div className={`${styles.bodySection} ${styles.createSection}`}>
-                    {infoBoxes.map((box) => (
-                        <div key={box.id} className={styles.infoBox}>
-                            <input type="text" placeholder={box.text}></input>
-                        </div>
-                    ))}
-                    <div className={styles.form}>
-                        <form>
-                            <input type="checkbox" id="marketing" value="Yes"/>
-                            <label> I am interested in future marketing</label><br/>
-                        </form>
-                    </div>
+                    <form>
+                        {infoBoxes.map((box) => (
+                            <div key={box.id} className={styles.infoBox}>
+                                <input type="text" name={box.id} placeholder={box.text} required={box.id !== 'phone'}></input>
+                            </div>
+                        ))}
+                        <input type="checkbox" id="marketing" value="Yes"/>
+                        <label> I am interested in future marketing</label><br />
+                        <button className="submit-button" type="submit">CREATE <br/> ACCOUNT</button>
+                    </form>
                 </div>
+
                 <div className={`${styles.bodySection} ${styles.reqsSection}`}>
                     <p>
                         Password Must Contain at least: <br/>
                         - 1 Uppercase Letter <br/>
                         - 1 Lowercase Letter <br/>
-                        - 1 Number Value {'(0 - 9)'} <br/>
-                        - 1 Special Character {'(#,$,%,&,!)'} <br/>
-                        - Minimum of 8 Characters <br/>
+                        - 1 Number (0 - 9) <br/>
+                        - 1 Special Character (#,$,%,&,!) <br/>
+                        - 8 Characters Minimum<br/>
                     </p>
+                    <button className="submit-button" type="submit"><a href='/login'>BACK TO LOGIN</a></button>
+                    
                 </div>
             
             {/* <AccountCollecter currentPage = "/signup" /> */}
