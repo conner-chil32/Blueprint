ebe50d7e88cf3944f1c4ff51fe1fc16d519bc224
Elijah <145163006+ElijahSource@users.noreply.github.com>
2025-09-09 15:32:31 -0700

    Fixed fixing of connection file

--- blueprint/lib/connection.js
+++ blueprint/lib/connection.js
@@ -1,32 +1,7 @@
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2';
 import { validateConnection } from './utility.js';
 
-
-/*
-    getConnectionObject: A function that returns the connection object to the database.
-    Input: none
-    Output: global.connection - The connection object to the database.
-    Date: 4/13/2025
-    Author: Lydell Jones
-    Dependencies: mysql
-*/
-export async function getConnectionObject() {
-    console.log("[DB] Getting connection object...");
-    return await openConnection(3); //: GLOBAL.CONNECTION IS SET HERE
-}
-
-/*
-    openConnection: A function that opens a connection to the database if one does not already exist.
-    Input: none
-    Ouput: global.connection - The connection object to the database.
-    Date: 4/13/2025
-    Author: Lydell Jones
-    Dependencies: mysql
-*/
-export async function openConnection(attempts = 1) {
-    if (attempts <= 0) return 0;
-    console.log("[DB] Creating connection to database...");
-    var connection = await mysql.createPool({
+export const connection = await mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
@@ -36,15 +11,6 @@ export async function openConnection(attempts = 1) {
     connectionLimit: 10,
     }).promise();
 
-    if (connection) {
-        console.log("[DB] Connection to database created successfully.");
-        return connection;
-    } else {
-        console.log("[DB] Error creating connection to database. Retrying...");
-        return await openConnection(attempts - 1);
-    }
-}
-
 /*
     closeConnection: A function that closes the connection to the database if one exists.
     Input: none
@@ -53,26 +19,33 @@ export async function openConnection(attempts = 1) {
     Author: Lydell Jones
     Dependencies: mysql
 */
-export async function closeConnection(connection) {
-    const result = false;
-    
-    if (result = !await validateConnection(connection)) {
-        console.log("[DB] No connection to close.")
-    } 
-    try{
-        console.log("[DB] Closing connection to database...");
-        try {
-            await connection.releaseConnection();
-        } catch (error) {
-            throw new Error("[DB] Error on closing the connection.")
-        }
+export async function closeConnection() {
+    try {
+        await validateConnection();
+        await connection.end();
     } catch (err) {
-        console.log("[DB] Error on close... Shutting down.", err)
-        return await shutdownConnection(connection);
+        throw false;
     }
+    return true;
+    
+       
+    // if (result = !await validateConnection(connection)) {
+    // } 
+    // try{
+    //     console.log("[DB] Closing connection to database...");
+    //     try {
+    //         await connection.releaseConnection();
+    //     } catch (error) {
+    //         throw new Error("[DB] Error on closing the connection.")
+    //     }
+    // } catch (err) {
+    //     console.log("[DB] Error on close... Shutting down.", err)
+    //     return await shutdownConnection(connection);
+    // }
 }
 
 /*
+    ##### OBSOLETE FUNCTION DO NOT USE IT WILL NOT WORK #####
     shutdownConnection: A function that destroys the connection to the database if one exists.
     Input: none
     Output: deletion of global.connection - The connection object to the database.
@@ -80,20 +53,20 @@ export async function closeConnection(connection) {
     Author: Lydell Jones
     Dependencies: mysql
 */
-async function shutdownConnection(connection) {
-    if (!await validateConnection(connection)) return true;
-    if (result) {
-        try{
-            console.log("[DB] Attempting to destroy connection")
-            await connection.destroy();
-        } catch (err) {
-            console.log("[DB] Error destroying connection to database:", err);
-            return false;
-        }
-    } else {
-        console.log("[DB] No connection to destroy.");
-        return true;
-    }
-    return true;
-}   
+// async function shutdownConnection() {
+//     if (!await validateConnection()) return true;
+//     if (result) {
+//         try{
+//             console.log("[DB] Attempting to destroy connection")
+//             await connection.destroy();
+//         } catch (err) {
+//             console.log("[DB] Error destroying connection to database:", err);
+//             return false;
+//         }
+//     } else {
+//         console.log("[DB] No connection to destroy.");
+//         return true;
+//     }
+//     return true;
+// }   
 
