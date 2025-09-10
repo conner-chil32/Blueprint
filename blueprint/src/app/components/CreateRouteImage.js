bee4add1b77390b3bc7d7942100156a1ee916fc7
Aaron Goodlund <agoodlund@csus.edu>
2025-09-10 12:45:32 -0700

    SCRUM-39: update pricing page to be easier to read and edit. additionally updated components/CreateRouteImage.js to accept image size and alt text as well as adding 3 classes to globals.css to have more control over text capitalization when printed

--- blueprint/src/app/components/CreateRouteImage.js
+++ blueprint/src/app/components/CreateRouteImage.js
@@ -11,7 +11,8 @@ outputs:
     next.js button
 
 This function uses premade dictionaries create image buttons to reuse code as much as possible
-does not currently support setting image dimensions or alt text
+~~does not currently support setting image dimensions or alt text~~
+9/10/25: Supports setting image dimensions and alt text to be desplayed if the image cannot load
 */
 export default function CreateImage({code = ""}){
     const path = library[code]
@@ -21,7 +22,8 @@ export default function CreateImage({code = ""}){
 
     return(
         <div className = "routingImage">
-            <Link href={path.href} src={path.image} className={path.className}>
+            <Link href={path.href}><img title = {path.label} src = {path.image} width = {path.width} height = {path.height} alt = {path.alt_text} className={path.className}></img>
+                {path.label} 
             </Link>
         </div> 
     )
