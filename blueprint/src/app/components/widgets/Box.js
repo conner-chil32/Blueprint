3af29b7ee4c38ca1e0a42e54f3f47963c6d7386d
Conner Childers <fishbacon561@gmail.com>
2025-10-02 19:58:30 -0700

    fixing lost files

--- blueprint/src/app/components/widgets/Box.js
+++ blueprint/src/app/components/widgets/Box.js
@@ -1,12 +1,5 @@
 import { Widget } from './Widget';
 
-/** Christopher Parsons, 9/18/2025
- * Inputs:
- *  scale: number
- *  ...props: Attributes from Widget
- * 
- * A widget that, when on the screen, appears as a box.
- */
 export function Box({ scale, ...props }) {
   
   return (
--- blueprint/src/app/components/widgets/Widget.js
+++ blueprint/src/app/components/widgets/Widget.js
@@ -1,63 +1,39 @@
 import { Rnd } from 'react-rnd';
+import { useRef } from 'react';
 
-/** Christopher Parsons, 9/18/2025
- * Inputs:
- *  id: number
- *  x: number
- *  y: number
- *  width: number
- *  height: number
- *  isSelected: Boolean
- *  isMoving: Boolean
- *  rotation: number
- *  style: CSS attributes
- *  onClick: function
- *  alertDragStop: function
- *  onDragStart: function
- *  onDragStop: function
- *  scale: number
- * 
- * A widget component holding all the information about its own position, movement, and every attribute.
- * A base funciton for other widget types.
- */
-export function Widget({ staticRender, id, x, y, width, height, isSelected, isMoving, rotation, style = {}, onClick, alertDragStop, children, pointerEventsNone, onDragStart, onDragStop, scale }) {
+export function Widget({ id, x, y, width, height, isSelected, isMoving, rotation, style = {}, onClick, alertDragStop, children, pointerEventsNone, onDragStart, onDragStop, scale }) {
 
   const handleResize = (e, direction, refToElement, delta, position) => {
-    alertDragStop && alertDragStop(id, position.x, position.y, { width: parseInt(refToElement.style.width, 10), height: parseInt(refToElement.style.height, 10) });
+    alertDragStop && alertDragStop(id, position.x, position.y, {width: parseInt(refToElement.style.width, 10), height: parseInt(refToElement.style.height, 10)});
   };
-
-  /** Christopher Parsons 9/26/2025
-   * Return the Rnd and the div if it is in the canvas page. If
-   * html is currently being exported, just return the div.
-   */
-  if (!staticRender) {
+    
     return (
-      <Rnd
-        position={{ x, y }}
-        size={{ width: width, height: height }}
-        bounds="parent"
-        // Update live
-        onResize={(e, direction, refToElement, delta, position) => {
-          handleResize(e, direction, refToElement, delta, position);
-          // Alert the canvas page when resizing starts
-          onDragStart && onDragStart(id);
-        }}
-        // Finish resizing
-        onResizeStop={(e, direction, refToElement, delta, position) => {
-          handleResize(e, direction, refToElement, delta, position);
-          // Alert the canvas page when resizing stops
-          onDragStop && onDragStop(id);
-        }}
-        onDragStart={(e) => {
-          // Alert the canvas page when dragging starts
-          onDragStart && onDragStart(id);
-        }}
-        onDragStop={(e, data) => {
-          alertDragStop && alertDragStop(id, data.x, data.y);
-          // Alert the canvas page when dragging stops
-          onDragStop && onDragStop(id);
-        }}
-        scale={scale}
+    <Rnd
+      position = {{x, y}}
+      size={{ width: width, height: height }}
+      bounds="parent"
+      // Update live
+      onResize={(e, direction, refToElement, delta, position) => {
+        handleResize(e, direction, refToElement, delta, position);
+        // Alert the canvas page when resizing starts
+        onDragStart && onDragStart(id);
+      }}
+      // Finish resizing
+      onResizeStop={(e, direction, refToElement, delta, position) => {
+        handleResize(e, direction, refToElement, delta, position);
+        // Alert the canvas page when resizing stops
+        onDragStop && onDragStop(id);
+      }}
+      onDragStart={(e) => {
+        // Alert the canvas page when dragging starts
+        onDragStart && onDragStart(id);
+      }}
+      onDragStop = {(e, data) => {
+        alertDragStop && alertDragStop(id, data.x, data.y);
+        // Alert the canvas page when dragging stops
+        onDragStop && onDragStop(id);
+      }}
+      scale={scale}
       >
         <div
           key={id}
@@ -71,36 +47,15 @@ export function Widget({ staticRender, id, x, y, width, height, isSelected, isMo
             pointerEvents: pointerEventsNone ? 'none' : 'auto',
             ...style,
           }}
-          onClick={e => {
-            // Prevent click from bubbling up to the canvas
-            e.stopPropagation();
-            //console.log('Stopping propagation');
-            onClick && onClick(e);
-          }}
-        >
-          {children}
-        </div>
-      </Rnd>
-    );
-  } else {
-    return (
-      <div
-        key={id}
-        style={{
-          position: "absolute",
-          left: x,
-          top: y,
-          transform: `rotate(${rotation || 0}deg)`,
-          width: width,
-          height: height,
-          border: '1px solid black',
-          isMoving: isMoving,
-          pointerEvents: pointerEventsNone ? 'none' : 'auto',
-          ...style,
+        onClick={e => {
+          // Prevent click from bubbling up to the canvas
+          e.stopPropagation();
+          //console.log('Stopping propagation');
+          onClick && onClick(e);
         }}
       >
         {children}
       </div>
-    );
-  }
+    </Rnd>
+  );
 }
