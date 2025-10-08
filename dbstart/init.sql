2a0a745aa6d43ce363aeb87c2c10cfbd39caf0ea
David Vigil <160567397+staticBytess@users.noreply.github.com>
2025-10-07 22:13:22 -0700

    added new tables and columns

--- dbstart/init.sql
+++ dbstart/init.sql
@@ -16,12 +16,36 @@ CREATE TABLE IF NOT EXISTS userAccounts (
 );
 
 CREATE TABLE IF NOT EXISTS userWebsites (
-    id INT NOT NULL AUTO_INCREMENT,
+    siteID INT NOT NULL AUTO_INCREMENT,
     userId INT NOT NULL,
     websiteName VARCHAR(255) NOT NULL,
-    websiteDateAdded TIMESTAMP NOT NULL DEFAULT NOW(),
-    websiteDateLastVisited TIMESTAMP DEFAULT NOW(),
+    websiteDateCreated TIMESTAMP NOT NULL DEFAULT NOW(),
     websiteDateLastModified TIMESTAMP DEFAULT NOW(),
+    websitePageCount INT NOT NULL,
+    webisteStatus ENUM('active', 'inactive',) NOT NULL DEFAULT 'inactive';
+    isAdmin BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (userId) REFERENCES userAccounts(userId),
-    PRIMARY KEY (id)
+    PRIMARY KEY (siteID)
+);
+
+CREATE TABLE IF NOT EXISTS websitePages (
+    pageID INT NOT NULL AUTO_INCREMENT,
+    siteID INT NOT NULL,
+    pagePath VARCHAR(255) NOT NULL,
+    pageData JSON, -- JSON so html, css, jc, etc. can be bundled together
+    pageLastAccessed TIMESTAMP DEFAULT NOW(),
+    UNIQUE KEY (siteID, pagePath), -- Ensures its a unqiue path for this website (but the same path can still be used on other websites)
+    FOREIGN KEY (siteID) REFERENCES userWebsites(siteID) ON DELETE CASCADE,
+    PRIMARY KEY (pageID)
+);
+
+CREATE TABLE IF NOT EXISTS websiteMedia (
+    mediaID INT NOT NULL AUTO_INCREMENT,
+    siteID INT NOT NULL,
+    mediaLastAccessed TIMESTAMP DEFAULT NOW(),
+    fileType ENUM('image', 'video', 'audio') NOT NULL,
+    mimeType VARCHAR(255) NOT NULL,
+    fileData LONGBLOB NOT NULL,
+    FOREIGN KEY (siteID) REFERENCES userWebsites(siteID) ON DELETE CASCADE,
+    PRIMARY KEY (mediaID)
 );
