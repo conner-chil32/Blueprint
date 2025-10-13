207f5ab73f2602f721e36d6202ddec97210e9546
LJJones123 <124975239+LJJones123@users.noreply.github.com>
2025-10-13 11:25:34 -0700

    fixed login, attaches cookie that will signify login and attach the user id to the user for subsequent requests

--- blueprint/src/app/api/page/route.js
+++ blueprint/src/app/api/page/route.js
@@ -1,5 +1,32 @@
-export default function GET() {
-    console.log("[site] Attempting to retrieve page");
+import { updateSite } from "@lib/siteQueries";
+import { NextResponse } from "next/server";
+import { getCookie } from "../CookieController";
 
+export default function GET(req) {
+    
+}
+
+export default async function PUT(req) {
+    try{
+        const { name, siteID } = await request.json();
+        userID = getCookie(req,'UserCookie');
+
+        if (userID === undefined || userID == "") {
+            return NextResponse({success: true}, {status: 200});
+        }
+
+        await updateSite();
+
+        return NextResponse({success: true}, {status: 200});
+    } catch (err) {
+    }
+    
+}
+
+export default function POST(req) {
+    
+}
+
+export default function DELETE(req) {
     
 }
