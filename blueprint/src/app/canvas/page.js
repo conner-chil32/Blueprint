64a961b56cb8df8987b066cdcd2203b1c0d2a421
Angel Ramirez <146392572+AngelinhoRG@users.noreply.github.com>
2025-10-12 21:25:22 -0700

    SCRUM-306-308-315: Widget Refinements

--- blueprint/src/app/canvas/page.js
+++ blueprint/src/app/canvas/page.js
@@ -363,6 +363,7 @@ export default function CanvasPage() {
           objectFit: 'cover',
           showBorder: true,
           borderColor: '#333333',
+          adSnippet: '',
         };
         break;
 
