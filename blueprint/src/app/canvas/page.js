d52aecaf7fba588ee50e243efe6ba0cc1638ae1c
LJJones123 <124975239+LJJones123@users.noreply.github.com>
2025-11-09 21:24:31 -0800

    SCRUM-320 allows for multiple pages

--- blueprint/src/app/canvas/page.js
+++ blueprint/src/app/canvas/page.js
@@ -333,7 +333,7 @@ export default function CanvasPage() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(pages[0]),
+        body: JSON.stringify(pages),
         redirect: 'manual'
       });
 
