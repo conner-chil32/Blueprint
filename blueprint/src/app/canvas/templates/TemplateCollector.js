8a0edc49a3463f2ef5e6eecb16c201359038025d
Aaron Goodlund <agoodlund@csus.edu>
2025-11-21 11:38:04 -0800

    SCRUM-367: Canvas Templates

---
+++ blueprint/src/app/canvas/templates/TemplateCollector.js
@@ -0,0 +1,27 @@
+
+//each template made saved by link name so it can be loaded from anywhere
+//make each template on the canvas and then save as .json and put in the templates folder
+
+const templates = [
+        //the name and file location are separate so the name can be changed in code without needing to rename the file itself
+    {
+        name: "default",
+        location: "templates/default"
+    },
+    {
+        name: "blog",
+        location: "templates/blog"
+    },
+    {
+        name: "bulletin",
+        location: "templates/bulletin"
+    },
+    {
+        name: "gallery",
+        location: "templates/gallery"
+    }
+];
+export { templates };
+
+
+//TODO: Make as many quality templates as possible
